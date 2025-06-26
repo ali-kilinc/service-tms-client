1bba31d10d789bd493ad5136d1a8291c5be658a1
ali-klnc <ali.kilinc@udemy.com>
2025-06-26 13:17:09 +0300

    update client as kotlin service

---
+++ src/main/kotlin/com/example/hello/DemoApplication.kt
@@ -0,0 +1,26 @@
+package com.example.hello
+
+import com.udemy.libraries.l10n.gettext
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class HelloController {
+	private val greetings = listOf(
+		gettext("Welcome"),
+		gettext("Hello, World!"),
+		gettext("This is a sample demo application to test translations.")
+	)
+
+	@GetMapping("/hello")
+	fun sayHello(): List<String> = greetings
+}
